27346ed93a476c5c95ee5d674e526a1e7c3e8496
lisen <lisensaka>
2023-08-26 12:57:24 +0200

    first assignment, creating and styling components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,19 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ServerComponent } from './servers/server/server.component';
+import { ServersComponent } from './servers/servers.component';
+import { WarningAlertComponent } from './assignment1/warning-alert/warning-alert.component';
+import { SuccessAlertComponent } from './assignment1/success-alert/success-alert.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ServerComponent,
+    ServersComponent,
+    WarningAlertComponent,
+    SuccessAlertComponent
   ],
   imports: [
     BrowserModule
