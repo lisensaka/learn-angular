e2bb4440ec13f6cdf37ff21ef0eb677e77215292
lisen <lisensaka>
2023-08-29 22:11:26 +0200

    app-initialization-component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,25 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
+import { ShopingListComponent } from './shoping-list/shoping-list/shoping-list.component';
+import { ShopingListEditComponent } from './shoping-list/shoping-list-edit/shoping-list-edit.component';
+import { RecipeListComponent } from './recipe-book/recipes/recipe-list/recipe-list.component';
+import { RecipeItemComponent } from './recipe-book/recipes/recipe-list/recipe-item/recipe-item.component';
+import { RecipeDetailComponent } from './recipe-book/recipes/recipe-detail/recipe-detail.component';
+import { HeaderComponent } from './header/header/header.component';
+import { RecipesComponent } from './recipe-book/recipes/recipes.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ShopingListComponent,
+    ShopingListEditComponent,
+    RecipeListComponent,
+    RecipeItemComponent,
+    RecipeDetailComponent,
+    HeaderComponent,
+    RecipesComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/shoping-list/shoping-list-edit/shoping-list-edit.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-shoping-list-edit',
+  templateUrl: './shoping-list-edit.component.html',
+  styleUrls: ['./shoping-list-edit.component.css']
+})
+export class ShopingListEditComponent {
+
+}
